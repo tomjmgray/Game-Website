43f9c818a8b4696785996fd25bd2c5efc235e607
tomjmgray <tomjmgray96@gmail.com>
2021-04-14 14:09:45 -0700

    Adds form to create lobby page

--- game-website/src/pages/CreateLobby.js
+++ game-website/src/pages/CreateLobby.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 class CreateLobby extends React.Component {
     state = {
-        lobbyCode: null,
-        username: null,
+        lobbyCode: '',
+        username: '',
+        gameType: 'Texas Holdem',
+        openLobby: false,
+        startingChips: 1000,
+        smallBlind: 5,
+        bigBlind: 10
     };
 
     generateLobby = () => {
@@ -13,6 +18,15 @@ class CreateLobby extends React.Component {
             result.push(chars.charAt(Math.floor(Math.random() * chars.length)))
         };
         return result.join('');
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.id]: event.target.value})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
     }
 
     componentDidMount(){
@@ -27,6 +41,27 @@ class CreateLobby extends React.Component {
             <div>
                 <h1>New Lobby</h1>
                 <h2>Lobby Code: {this.state.lobbyCode}</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <br/>
+                    <input type="text" name="username" id="username" value={this.state.username} onChange={this.handleChange} required></input>
+                    <br/>
+                    <label htmlFor="startingChips">Starting Chips</label>
+                    <br/>
+                    <input type="number" name="startingChips" id="startingChips" value={this.state.startingChips} onChange={this.handleChange}></input>
+                    <br/>
+                    <label htmlFor="smallBlind">Small Blinds</label>
+                    <br/>
+                    <input type="number" name="smallBlind" id="smallBlind" value={this.state.smallBlind} onChange={this.handleChange}></input>
+                    <br/>
+                    <label htmlFor="bigBlind">Big Blinds</label>
+                    <br/>
+                    <input type="number" name="bigBlind" id="bigBlind" value={this.state.bigBlind} onChange={this.handleChange}></input>
+                    <br/>
+                    <br/>
+                    <button type="submit">Create Lobby</button>
+
+                </form>
             </div>
             
         )
